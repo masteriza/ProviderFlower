b0a7a735432efba4e16a611221a34937ad0cd1ab
masteriza <masteriza@ukr.net>
2015-07-29 18:06:40 +0300

    15.07.29 work1

---
+++ src/main/java/ConnectionConfiguration.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+/**
+ * Created by user on 29.07.2015.
+ */
+public class ConnectionConfiguration {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by user on 29.07.2015.
+ */
+public class main {
+}
